dd460fc1ab469e95f948ac8bc69379eb7f49e985
fcolls <collselectronicsprojects@gmail.com>
2022-03-06 22:00:45 +0100

    Commit inicial

---
+++ src/cuentas/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuentas;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = operativa_cuenta(2500);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
+
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+
+    private static CCuenta operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789", cantidad,0);
+        return cuenta1;
+    }
+}
